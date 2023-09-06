edc5d450a031e9151dc72ff8da50c054b3f9956f
Kyle Hughes <kylehughes@students.abtech.edu>
2023-09-06 11:24:41 -0400

    Created a birc class

---
+++ asgn01/bird-challenge/index.php
@@ -0,0 +1,20 @@
+<?php
+  class Bird {
+
+    var $commonName;
+    var $food;
+    var $nestPlacement;
+    var $conservationLevel;
+
+    function song() {
+      return ".";
+    }
+
+    function canFly() {
+      return ".";
+    }
+
+  }
+
+
+?>
