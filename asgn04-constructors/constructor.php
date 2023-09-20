711637f643621c9d1f08e2fa71b8230baef83dc5
Kyle Hughes <kylehughes@students.abtech.edu>
2023-09-20 13:29:06 -0400

    Constructor complete

--- asgn04-constructors/constructor.php
+++ asgn04-constructors/constructor.php
@@ -1,3 +1,21 @@
 <?php
+Class Bird {
 
+  public $commonName;
+  public $latinName;
+  public function __construct($args=[]) {
+    $this->commonName = $args['commonName'] ?? NULL;
+    $this->latinName = $args['latinName'] ?? NULL;
+  }
+}
+
+$robin = new Bird(["commonName"=>"American Robin", "latinName"=>"Turdus migratorius"]);
+$towhee = new Bird(["commonName"=>"Eastern Towhee", "latinName"=>"Pupilo erythrophthalmus"]);
+
+
+echo "Common name: " . $robin->commonName . "<br>";
+echo "Common name: " . $robin->latinName . "<br>";
+echo "<hr>";
+echo "Common name: " . $towhee->commonName . "<br>";
+echo "Common name: " . $towhee->latinName . "<br>";
 ?>
