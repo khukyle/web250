0f646d67e735e1b23307bb13341cc35ae0f4c76c
Kyle Hughes <kylehughes@students.abtech.edu>
2023-09-26 22:53:46 -0400

    with autoload

---
+++ asgn04-constructors/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+  function my_autoload($class) {
+    if(preg_match('/\A\w+\Z/', $class)) {
+    include 'classes/' . $class . '.class.php';
+    }
+  }
+  spl_autoload_register('my_autoload');
+
+  $flyCatcher = new Bird(["commonName"=>"Acadian Flycatcher", "latinName"=>"Empidonax virescens"]);
+  echo $flyCatcher->commonName;
+?>
--- asgn04-constructors/constructor-arguments.php
+++ asgn04-constructors/constructor-arguments.php
@@ -1,4 +1,7 @@
 <?php
+
+use Bird as GlobalBird;
+
 Class Bird {
 
   public $commonName;
@@ -11,10 +14,19 @@ Class Bird {
 
 $robin = new Bird(["commonName"=>"American Robin", "latinName"=>"Turdus migratorius"]);
 $towhee = new Bird(["commonName"=>"Eastern Towhee", "latinName"=>"Pupilo erythrophthalmus"]);
+$flycatcher = new Bird(["commonName"=>"Acadian Flycatcher", "latinName"=>"Turdus migratorius"]);
+$wren = new Bird(["commonName"=>"Carolina Wren", "latinName"=>"Thryothorus ludovicianus"]);
 
 echo "Common name: " . $robin->commonName . "<br>";
 echo "Common name: " . $robin->latinName . "<br>";
 echo "<hr>";
 echo "Common name: " . $towhee->commonName . "<br>";
 echo "Common name: " . $towhee->latinName . "<br>";
+echo "<hr>";
+echo "Common name: " . $flycatcher->commonName . "<br>";
+echo "Common name: " . $flycatcher->latinName . "<br>";
+echo "<hr>";
+echo "Common name: " . $wren->commonName . "<br>";
+echo "Common name: " . $wren->latinName . "<br>";
+echo "<hr>";
 ?>
